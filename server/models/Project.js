6db7a52b8196d0e6947ef5cec677620ba160e49a
Vaishnavi J K <upadyayavaishnavi10@gmail.com>
2026-03-06 20:46:12 +0530

    feat(project):create project schema

---
+++ server/models/Project.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const projectSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    projectType: {
+      type: String,
+      required: true,
+      enum: ["restaurant", "portfolio", "ecommerce"],
+    },
+    currentStage: {
+      type: Number,
+      default: 1,
+    },
+    completedStages: {
+      type: [Number],
+      default: [],
+    },
+    progressPercentage: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  { timestamps: true },
+);
+module.exports = mongoose.model("Project", projectSchema);
